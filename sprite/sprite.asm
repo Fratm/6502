ddcece4e60d1c44ebe1f53ddbe7fae285d8c5794
Fratm <fratm@fratm.fratm.com>
2024-03-29 09:38:27 -0700

    Comments and clean up

--- sprite/sprite.asm
+++ sprite/sprite.asm
@@ -1,11 +1,10 @@
 // Sprite demo written in assembly by Fratm
 // This is the demo from the C64 users guide
-// But I re-wrote it in assembly.
+// But I re-wrote it in assembly.  Release to public domain 
 
 // constants 
 .const VICII = $D000
 .const SPRITEBANK = $0340
-
 
 // The basic header stuff.
 * = $0801
@@ -15,8 +14,8 @@
     .text "2064"
     .byte 0, 0
 
+// Start 
 * = $0810
-// set speed
 
 // Enable Sprite #2
     lda #$0004
@@ -50,8 +49,9 @@ moveloop:
     cmp #$00C8
     bne moveloop
 
-
-// You can change the speed by poking a value into $002A (42 decimal)
+// poke 42, 55 is a good smooth speed.  This is the same dealy routine
+// I used in my simple scroller program, but added a variable speed control.
+// change delay time with poke 42, ##  (Lower the ## the faster the scroll)
 delay:
     stx bufferx
     sty buffery
@@ -67,16 +67,16 @@ innerLoop:
     ldy buffery
     rts               // Return from subroutine
 
-
 // Just some register buffers -- I'm new a this, im sure there is a better way.
 bufferx: .byte 00
 buffery: .byte 00
 buffera: .byte 00
 
+// probably a better way then to allocate this byte, but I'm still new and this made sense.
 counter:
     .byte 00
 
-
+// Sprite data in decimal, copied from the book.
 sprite:
     .byte 0,127,0,1,255,192,3,255,224,3,231,224
     .byte 7,217,240,7,223,240,7,217,240,3,231,224
